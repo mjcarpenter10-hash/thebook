3f5f8b6c942651f30910e5dd60021b0325070ac8
mjcarpenter10-hash <mjcarpenter10@gmail.com>
2026-09-14 15:07:37 -0500

    The Book 1.0

---
+++ sw.js
@@ -0,0 +1,28 @@
+var CACHE = "wordfamilies-v12";
+var ASSETS = ["./", "./index.html", "./story.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; })
+                          .map(function(k){ return caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+
+self.addEventListener("fetch", function(e){
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      if (hit) return hit;
+      return fetch(e.request).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, copy); }).catch(function(){});
+        return res;
+      }).catch(function(){ return caches.match("./index.html"); });
+    })
+  );
+});
